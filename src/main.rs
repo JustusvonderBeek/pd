727ddb8d410186ae5b041827dafaf1cdbd96f6e3
Justus von der Beek <beek@in.tum.de>
2021-07-15 23:40:48 +0100

    Read out connection ID

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod packets;
 
 use std::{time, env, io};
 use std::error::Error;
+use byteorder::BigEndian;
 use tokio::net::UdpSocket;
 use crate::cmdline_handler::*;
 
@@ -34,25 +35,38 @@ async fn start_server(opt : Options) -> Result<(), Box<dyn Error>> {
     loop {
         let (len, addr) = sock.recv_from(&mut buf).await.unwrap();
         println!("Received {} bytes from {}", len, addr);
+
+        let rawConnID : [u8; 4] = [0, buf[0], buf[1], buf[2]];
+        let connID : u32 = u32::from_be_bytes(rawConnID);
+        println!("Read connID: {:x}", connID);
+
+        if connID == 0 {
+            // In this case the client is new
+            
+        } else {
+            // The client already exists and we need to distribute the
+            // packet to the correct thread/task ...
+
+        }
     } 
 
     // I would like to move this into its own thread but 
     // I guess for UDP this wont be possible because we just have a
     // socket but not an individual handle per client or connection?
-    
+
     // tokio::spawn(async move {
-    //     server_task(&sock).await;
+    //     server_task().await;
     // });
     
-    Ok(())
+    // Ok(())
 }
 
-async fn server_task(socket : &UdpSocket) {
+async fn server_task() {
     let mut buf : [u8; 1500] = [0; 1500];
     
     loop {
-        let (len, addr) = socket.recv_from(&mut buf).await.unwrap();
-        println!("Received {} bytes from {}", len, addr);
+        // let (len, addr) = socket.recv_from(&mut buf).await.unwrap();
+        // println!("Received {} bytes from {}", len, addr);
     } 
 
 }
