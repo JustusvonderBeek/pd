47e94b38ea7a9d4a09992bf88768ac1cb1ba0ffa
Justus von der Beek <beek@in.tum.de>
2021-07-31 14:06:21 +0200

    Added writing to file

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,15 @@ extern crate simplelog;
 use std::{time, env, io};
 use simplelog::*;
 use std::fs::File;
+use std::io::Write;
+use std::fs::OpenOptions;
 use std::fs;
 use pretty_hex::*;
 use std::error::Error;
 use std::net::UdpSocket;
 use std::collections::HashSet;
 use byteorder::BigEndian;
+use rand::prelude::*;
 use crate::cmdline_handler::*;
 
 enum PacketType {
@@ -61,12 +64,18 @@ fn start_server(opt : &Options) {
 
     loop {
         // TODO: Error handling
+        // 1. Reading a new packet (because this is not threaded block here)
         let (len, addr) = sock.recv_from(&mut buf).expect("Failed to receive data");
-        info!("Received {} bytes from {}", len, addr);
+        debug!("Received {} bytes from {}", len, addr);
+        
+        // 2. Check for a new connection
+
 
         let raw_conn_id : [u8; 4] = [0, buf[0], buf[1], buf[2]];
         let conn_id : u32 = u32::from_be_bytes(raw_conn_id);
         debug!("Read connID: {:x}", conn_id);
+
+        // 3. Parse the given packet
 
         if conn_id == 0 {
             // In this case the client is new
@@ -76,12 +85,9 @@ fn start_server(opt : &Options) {
             let conn_id = 10;
             conn_exists.insert(conn_id);
 
-
+            // 4.1. Handling a new client (Setup)
         } else {
-            // The client already exists and we need to distribute the
-            // packet to the correct thread/task ...
-            
-            break;
+           // 4.2. Handling an old client (Probably Ack or Metadata)
         }
     } 
     
@@ -89,7 +95,7 @@ fn start_server(opt : &Options) {
 
 fn test_functions() {
     handling_requests(PacketType::Request, &vec![1;1]);
-
+    handling_requests(PacketType::Data, &vec![1;1]);
 }
 
 fn handling_requests(p_type : PacketType, packet : &Vec<u8>) {
@@ -101,25 +107,40 @@ fn handling_requests(p_type : PacketType, packet : &Vec<u8>) {
             debug!("{}", pretty_hex(&file));
         },
         PacketType::Response => {
-
+            // Connection setup (we ack that we got the request and the file parameters)
         },
         PacketType::Data => {
-
+            // Transfer the next chunk of data to the client
+            let mut rnd = rand::thread_rng();
+            let mut nums : Vec<u8> = (0..100).collect();
+            nums.shuffle(&mut rnd);
+            debug!("{}", pretty_hex(&nums));
+            let mut output = OpenOptions::new().append(true).create(true).open("output.txt").expect("Failed to open file output.txt");
+            output.write_all(&nums).expect("Failed to append to file");
         },
         PacketType::Ack => {
-
+            // Got an ack from the client
+            
         },
         PacketType::Metadata => {
-
+            // This type of packet is never received by us !
         },
         PacketType::Error => {
-
+            // Closing the connection and freeing state
+            // conn_exists.remove(1);
         }
     }
 }
 
 fn start_client(opt : &Options) {
     // TODO: Implement client side
+    // 1. Request the file
+
+    // 2. Receive data in the loop
+
+    // 3. Ack the data and process the metadata
+
+    // 4. Close the connection after success
 }
 
 fn init_logger(opt : &Options) {
