7241214aafac2c0f448f7e8e13431cfc01af4c08
Justus von der Beek <beek@in.tum.de>
2021-08-05 13:59:05 +0100

    Started implementing receive data

--- src/client.rs
+++ src/client.rs
@@ -5,19 +5,20 @@ use std::fs::OpenOptions;
 use std::net::{UdpSocket, SocketAddr};
 use std::{thread, time};
 use std::result::Result;
+use std::collections::LinkedList;
 use crate::cmdline_handler::Options;
 use crate::packets::*;
 use crate::net_util::*;
 
 const PACKET_SIZE : u32 = 1280;
 const TIMEOUT : u64 = 1;
-const START_FLOW_WINDOW : u32 = 8;
+const START_FLOW_WINDOW : u16 = 8;
 
 pub struct TBDClient {
     options : Options,
     received : u64,
     connection_id : u32,
-    flow_window : u32,
+    flow_window : u16,
     block_id : u32,
     file_hash : [u8; 32],
     file_size : u64,
@@ -63,7 +64,14 @@ impl TBDClient {
             let request = RequestPacket::serialize(&0, &self.flow_window, &filename);
 
             // Sending the request to the server
-            send_data(&request, &sock, &servername);
+            match sock.send_to(&request, &servername) {
+                Ok(s) => debug!("Send {} bytes to {}", s, servername),
+                Err(e) => {
+                    error!("Failed to send to {}: {}", servername, e);
+                    // In case we fail we abort the sending
+                    return Err(e);
+                }
+            };
             info!("Requested file: {}", filename);
 
             // Receive response from server
@@ -77,6 +85,8 @@ impl TBDClient {
                 warn!("Received and error from the server: ErrorCode == {:x}", err.error_code);
                 continue;
             }
+
+            // TODO: Include handle for data packet!
 
             if !check_packet_type(&packet_buffer.to_vec(), PacketType::Response) {
                 error!("Expected a response packet but got something different!");
@@ -98,51 +108,7 @@ impl TBDClient {
             self.file_hash = res.file_hash;
             self.file_size = res.file_size;
 
-            // Create a buffer for the next block used to store incoming packets in the correct order
-            let buffer_size = PACKET_SIZE * self.flow_window;
-            let mut window_buffer = vec![0; buffer_size as usize];
-
-            let mut i = 0;
-            while i < self.flow_window {
-                // TODO: Error handling
-                let (addr, len) = sock.recv_from(&mut packet_buffer).expect("Failed to receive data from server");
-                
-                if check_packet_type(&packet_buffer.to_vec(), PacketType::Error) {
-                    error!("Received an error instead of a data packet!");
-                    break;
-                }
-
-                if !check_packet_type(&packet_buffer.to_vec(), PacketType::Data) {
-                    error!("Expected data packet but got something else!");
-                    continue;
-                }
-
-                let data = DataPacket::deserialize(&packet_buffer).unwrap();
-                if data.connection_id != self.connection_id {
-                    error!("Connection IDs do not match!");
-                    continue;
-                }
-
-                if data.block_id != self.block_id {
-                    warn!("Block IDs do not match");
-                    continue;
-                }
-
-                // Copying the data at the right place into the buffer
-                let start = data.sequence_id as usize * PACKET_SIZE as usize;
-                let end = start + PACKET_SIZE as usize;
-                window_buffer[start..end].copy_from_slice(&packet_buffer);
-
-                // TODO: Include the seq id in the ack
-
-
-                i += 1;
-            }
-            // TODO: Send the ACK
-
-
-            // Writing the current block in correct order into the file
-            self.write_data_to_file(&filename, &window_buffer).unwrap();
+            self.receive_data(&sock);            
         }
 
         info!("Finished file transfer");
@@ -179,8 +145,82 @@ impl TBDClient {
         }
     }
 
-    fn receive_next_block() {
+    fn create_ack_list(&self) -> LinkedList<u16> {
+        let list = LinkedList::new();
+        for i in 0..self.flow_window {
+            list.push_back(i);
+        }
+        list
+    }
 
+    fn receive_data(&self, sock : &UdpSocket) {
+        // Compute the size of the whole next block
+        let buffer_size = PACKET_SIZE * self.flow_window as u32;
+        let mut window_buffer = vec![0; buffer_size as usize];
+        
+        let mut packet_buffer : [u8; PACKET_SIZE as usize] = [0; PACKET_SIZE as usize];
+        let mut i = 0;
+
+        loop {
+            // TODO: Error handling
+            let (len, addr) = self.receive_next(sock, &mut packet_buffer);
+            debug!("Received {} bytes from {}:\n{}", len, addr, pretty_hex(&packet_buffer));
+            
+            if check_packet_type(&packet_buffer.to_vec(), PacketType::Error) {
+                let err = match ErrorPacket::deserialize(&packet_buffer[0..len]) {
+                    Ok(e) => e,
+                    Err(e) => {
+                        error!("Failed to deserialize error packet: {}", e);
+                    }
+                };
+                error!("Received an error instead of a data packet: ErrorCode == {}", err.error_code);
+                // Abort the file transfer
+                std::process::exit(0);
+            }
+
+            if !check_packet_type(&packet_buffer.to_vec(), PacketType::Data) {
+                error!("Expected data packet but got something else!");
+                continue;
+            }
+
+            let data = match DataPacket::deserialize(&packet_buffer) {
+                Ok(d) => d,
+                Err(e) => {
+                    warn!("Failed to deserialize the data packet: {}", e);
+                    // This should be fixable in the retransmission (missing seq id)
+                    continue;
+                }
+            };
+
+            if data.connection_id != self.connection_id {
+                error!("Connection IDs do not match!");
+                send_error(&sock, &addr, ErrorTypes::Abort);
+                // Ignore
+                continue;
+            }
+
+            if data.block_id != self.block_id {
+                warn!("Block IDs do not match");
+                // Ignore
+                continue;
+            }
+
+            // Copying the data at the right place into the buffer
+            // - 1 because the seq id 0 is reserved for metadata but we still want to use the space
+            let start = (data.sequence_id - 1) as usize * PACKET_SIZE as usize;
+            // Copy only what we received
+            let end = start + len as usize;
+            window_buffer[start..end].copy_from_slice(&packet_buffer);
+
+            // TODO: Include the seq id in the ack
+
+
+            i += 1;
+            // break;
+        }
+
+        // Writing the current block in correct order into the file
+        // self.write_data_to_file(&filename, &window_buffer).unwrap();
     }
 
     fn handle_retransmission() {
