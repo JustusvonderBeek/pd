cbd38e0034f476de9d4e9fc0d26a994affcb8d11
Strodel, Axel <axel.strodel@aisec.fraunhofer.de>
2021-08-11 17:32:18 +0200

    Fix recognition of response and metadata packets

--- src/client.rs
+++ src/client.rs
@@ -137,7 +137,7 @@ impl TBDClient {
                 // debug!("Received response from {}: {}", addr, pretty_hex(&packet_buffer));
                 
                 // Check for errors and correct packet
-                let packet_type = get_packet_type_client(&packet, false);
+                let packet_type = get_packet_type_client(&packet[0..len], false);
                 debug!("Packet Type: {:?}", packet_type);
                 match packet_type {
                     PacketType::Error => {
@@ -174,6 +174,7 @@ impl TBDClient {
                             req = true;
                             offset = 0;
                             hash = Vec::new();
+                            debug!("Sent error");
                             send_error(&sock, &self.connection_id, &address, ErrorTypes::Abort);
                             
                             // Request the same file again...
